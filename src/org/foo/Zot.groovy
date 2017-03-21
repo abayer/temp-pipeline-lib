a7753970178e96d9b4ba34767b9caf46803014a3
Andrew Bayer <andrew.bayer@gmail.com>
2017-03-21 11:55:59 -0700

    Update Zot.groovy

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -1,15 +1,5 @@
 package org.foo;
 
-import org.jenkinsci.plugins.docker.workflow.Docker
-import groovy.transform.Field
-import org.jenkinsci.plugins.workflow.cps.CpsScript
-
-@Field Docker docker
-
-public Zot(script) {
-  this.docker = script.getProperty("docker")
-}
-
 def echo(msg) {
   echo "-> ${msg}"
 }
