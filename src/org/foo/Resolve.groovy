6cce2ad5e46c9ec508d14aaa43e571301eabfbe0
Andrew Bayer <andrew.bayer@gmail.com>
2017-03-30 16:03:53 -0700

    Update Resolve.groovy

--- src/org/foo/Resolve.groovy
+++ src/org/foo/Resolve.groovy
@@ -1,9 +1,9 @@
 package org.foo
 
 @GrabResolver(name='jenkins-internal',root="https://repo.jenkins-ci.org/releases/")
-@Grab(group="org.jenkinsci.plugins", module="pipeline-model-api", version="1.1.1")
+@Grab(group="org.jenkins-ci.plugins", module="housekeeper", version="1.1")
 public class Resolve implements Serializable {
   public String aThing() {
-    return "right there, a thing"
+    return org.jenkinsci.plugins.Housekeeper.getClass().getName()
   }
 }
