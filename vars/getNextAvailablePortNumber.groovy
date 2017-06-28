1809f3cc092146d47be8d5f6ff1780094fc982d8
Andrew Bayer <andrew.bayer@gmail.com>
2017-06-28 14:37:00 -0400

    Create getNextAvailablePortNumber.groovy

---
+++ vars/getNextAvailablePortNumber.groovy
@@ -0,0 +1,17 @@
+def call() {
+def portsInUseOnServer = {
+def cmd = """Invoke-Command `
+-ComputerName ${SERVER} `
+-ScriptBlock { `
+Import-Module WebAdministration; `
+Get-WebBinding | Select bindingInformation `
+}"""
+
+def output = cmd //powershell([script: cmd, returnStdout: true])
+def lines = output.split(/\n/).drop(3).dropRight(2)
+
+Pattern pattern = Pattern.compile(/\:(.*?)\:/)
+return lines.collect { pattern.matcher(it)[0][1].toInteger() }
+}
+portsInUseOnServer().each { println it }
+}
