37c1c17a05d6880cf60bf2247e59baee5a05296b
Andrew Bayer <andrew.bayer@gmail.com>
2018-04-13 14:57:45 -0400

    argh

--- vars/buildConfig.groovy
+++ vars/buildConfig.groovy
@@ -7,14 +7,21 @@ class buildConfig {
     }
     
     def addBuildParameters(buildParameters) {
-        //        paramList.addAll(steps.defaultParams())
+        def defaultParams = [
+            [$class: 'StringParameterDefinition', defaultValue: 'stable', description: 'Tag for ansible-executor image', name: 'ANSIBLE_EXECUTOR_TAG'],
+            [$class: 'StringParameterDefinition', defaultValue: 'stable', description: 'Tag for linchpin-executor image', name: 'LINCHPIN_EXECUTOR_TAG'],
+            [$class: 'StringParameterDefinition', defaultValue: '172.16.0.1:5000', description: 'Docker repo url for Openshift instance', name: 'DOCKER_REPO_URL'],
+            [$class: 'StringParameterDefinition', defaultValue: 'babelfish', description: 'Project namespace for Openshift operations', name: 'OPENSHIFT_NAMESPACE'],
+            [$class: 'StringParameterDefinition', defaultValue: 'pipeline', description: 'Service Account for Openshift operations', name: 'OPENSHIFT_SERVICE_ACCOUNT'],
+        ]
+        paramList.addAll(defaultParams)
         paramList.addAll(buildParameters)
     }
     
     def setBuildConfig() {
         def propList = []
         propList.addAll(otherProps)
-        propList.add(parameters(paramList))
+        propList.add([$class: 'ParametersDefinitionProperty', parameterDefinitions: paramList])
         properties(propList)
     }
 }
