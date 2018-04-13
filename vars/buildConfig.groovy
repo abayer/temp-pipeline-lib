bda088f35a33e6012790eb07b6b70b07131fe9dc
Andrew Bayer <andrew.bayer@gmail.com>
2018-04-13 14:39:43 -0400

    test

--- vars/buildConfig.groovy
+++ vars/buildConfig.groovy
@@ -7,13 +7,7 @@ class buildConfig {
   }
   
   def addBuildParameters(buildParameters) {
-    paramList.addAll([
-        string(defaultValue: 'stable', description: 'Tag for ansible-executor image', name: 'ANSIBLE_EXECUTOR_TAG'),
-        string(defaultValue: 'stable', description: 'Tag for linchpin-executor image', name: 'LINCHPIN_EXECUTOR_TAG'),
-        string(defaultValue: '172.16.0.1:5000', description: 'Docker repo url for Openshift instance', name: 'DOCKER_REPO_URL'),
-        string(defaultValue: 'babelfish', description: 'Project namespace for Openshift operations', name: 'OPENSHIFT_NAMESPACE'),
-        string(defaultValue: 'pipeline', description: 'Service Account for Openshift operations', name: 'OPENSHIFT_SERVICE_ACCOUNT'),
-    ])
+      paramList.addAll(defaultParams())
     paramList.addAll(buildParameters)
   }
   
@@ -23,4 +17,14 @@ class buildConfig {
     propList.add(parameters(paramList))
     properties(propList)
   }
+}
+
+private defaultParams() {
+    return [
+        string(defaultValue: 'stable', description: 'Tag for ansible-executor image', name: 'ANSIBLE_EXECUTOR_TAG'),
+        string(defaultValue: 'stable', description: 'Tag for linchpin-executor image', name: 'LINCHPIN_EXECUTOR_TAG'),
+        string(defaultValue: '172.16.0.1:5000', description: 'Docker repo url for Openshift instance', name: 'DOCKER_REPO_URL'),
+        string(defaultValue: 'babelfish', description: 'Project namespace for Openshift operations', name: 'OPENSHIFT_NAMESPACE'),
+        string(defaultValue: 'pipeline', description: 'Service Account for Openshift operations', name: 'OPENSHIFT_SERVICE_ACCOUNT'),
+    ]
 }
