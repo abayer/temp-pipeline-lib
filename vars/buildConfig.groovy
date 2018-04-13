21de4cc5c7e3ae35bf411153532358d1c7495657
Andrew Bayer <andrew.bayer@gmail.com>
2018-04-13 14:45:20 -0400

    scope

--- vars/buildConfig.groovy
+++ vars/buildConfig.groovy
@@ -2,17 +2,17 @@ def otherProps = []
 def paramList = []
 
 def addBuildConfiguration(buildConfiguration) {
-    otherProps.addAll(buildConfiguration)
+    this.otherProps.addAll(buildConfiguration)
 }
 
 def addBuildParameters(buildParameters) {
-    paramList.addAll(defaultParams())
-    paramList.addAll(buildParameters)
+    this.paramList.addAll(defaultParams())
+    this.paramList.addAll(buildParameters)
 }
 
 def setBuildConfig() {
     def propList = []
-    propList.addAll(otherProps)
-    propList.add(parameters(paramList))
+    propList.addAll(this.otherProps)
+    propList.add(parameters(this.paramList))
     properties(propList)
 }
