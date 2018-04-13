e30bf538c6d62f9a47afcb03ca861a25faf766af
Andrew Bayer <andrew.bayer@gmail.com>
2018-04-13 15:16:53 -0400

    debug log

--- vars/addBuildConfigurations.groovy
+++ vars/addBuildConfigurations.groovy
@@ -1,3 +1,5 @@
 def call(buildConfiguration) {
     buildConfig.configList.addAll(buildConfiguration)
+    echo "config: configlist: ${buildConfig.configList}"
+    echo "config: paramslist: ${buildConfig.paramList}"
 }
--- vars/addBuildParameters.groovy
+++ vars/addBuildParameters.groovy
@@ -1,3 +1,5 @@
 def call(buildParameters) {
     buildConfig.paramList.addAll(buildParameters)
+    echo "params: configlist: ${buildConfig.configList}"
+    echo "params: paramslist: ${buildConfig.paramList}"
 }
