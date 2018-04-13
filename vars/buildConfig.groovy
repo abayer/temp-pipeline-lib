71470632185b351d3f68aa21ebf408fec7676ba9
Andrew Bayer <andrew.bayer@gmail.com>
2018-04-13 14:44:30 -0400

    test

--- vars/buildConfig.groovy
+++ vars/buildConfig.groovy
@@ -1,20 +1,18 @@
-class buildConfig {
-  def otherProps = []
-  def paramList = []
-  
-  def addBuildConfiguration(buildConfiguration) {
+def otherProps = []
+def paramList = []
+
+def addBuildConfiguration(buildConfiguration) {
     otherProps.addAll(buildConfiguration)
-  }
-  
-  def addBuildParameters(buildParameters) {
-      paramList.addAll(defaultParams())
+}
+
+def addBuildParameters(buildParameters) {
+    paramList.addAll(defaultParams())
     paramList.addAll(buildParameters)
-  }
-  
-  def setBuildConfig() {
+}
+
+def setBuildConfig() {
     def propList = []
     propList.addAll(otherProps)
     propList.add(parameters(paramList))
     properties(propList)
-  }
 }
