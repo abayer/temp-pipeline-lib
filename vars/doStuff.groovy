37fde344ee91f9f73faa0ff2b7dafaaa23d48f0d
Andrew Bayer <andrew.bayer@gmail.com>
2018-11-26 16:24:09 -0500

    Create doStuff.groovy

---
+++ vars/doStuff.groovy
@@ -0,0 +1,23 @@
+def call(body) {
+    def config = ["hey": "there", "m": [:]]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def outerP = [:]
+    config.m.each { k, v ->
+        outerP[k] = { 
+            stage(k) {
+                echo "in outer ${k}";  
+                def innerP = [:]
+                v.each { i ->
+                    innerP[i] = { node { echo "starting inner ${i} for ${k}"; sh "sleep 15"; echo "ending inner ${i} for ${k}" } }
+                }
+                parallel innerP
+            }
+        }
+    }
+
+    checkpoint "Before parallel"
+    parallel outerP
+}
